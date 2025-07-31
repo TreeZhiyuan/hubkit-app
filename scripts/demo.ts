6882c54cea3f292b0d991bc40c45fe1b29601686
zhiyuan <czy20140714@163.com>
2025-07-31 19:54:41 +0800

    update github actions yml

--- scripts/demo.ts
+++ scripts/demo.ts
@@ -1,1 +1,21 @@
 console.log("This is a demo script for Hubkit App");
+
+
+import { TwitterApi } from 'twitter-api-v2';
+
+// Instantiate with desired auth type (here's Bearer v2 auth)
+const twitterClient = new TwitterApi(
+  {
+    appKey: '',
+    appSecret: '',
+    accessToken: '',
+    accessSecret: '',
+  }
+);
+
+// Tell typescript it's a readonly app
+const readOnlyClient = twitterClient.readOnly;
+
+// Play with the built in methods
+const user = await readOnlyClient.v2.userByUsername('czydmm');
+console.log(user);
