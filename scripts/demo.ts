12d9d20c649925488470527946f74219ca6b4a65
zhiyuan <czy20140714@163.com>
2025-08-01 11:07:58 +0800

    hidden sensitive info while deploy with github actions

--- scripts/demo.ts
+++ scripts/demo.ts
@@ -6,10 +6,10 @@ import { TwitterApi } from 'twitter-api-v2';
 // Instantiate with desired auth type (here's Bearer v2 auth)
 const twitterClient = new TwitterApi(
   {
-    appKey: '',
-    appSecret: '',
-    accessToken: '',
-    accessSecret: '',
+    appKey: process.env.APP_KEY || '',
+    appSecret: process.env.APP_SECRET || '',
+    accessToken: process.env.ACCESS_TOKEN || '',
+    accessSecret: process.env.ACCESS_SECRET || '',
   }
 );
 
