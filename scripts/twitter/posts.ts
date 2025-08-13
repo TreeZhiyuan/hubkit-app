0a1c26c57acbdc828bd1564fb572f9764150ac2f
Rye Gekel <zhiyuan_cui@yahoo.com>
2025-08-13 21:11:45 +0800

    Update posts.ts

--- scripts/twitter/posts.ts
+++ scripts/twitter/posts.ts
@@ -2,8 +2,8 @@ import { readOnlyClient, readWriteClient } from "./common.ts";
 
 // https://developer.x.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets
 
-// zhiha
-readOnlyClient.userTimeline('1397504889283092480', {
+// zhiha 1397504889283092480
+readOnlyClient.userTimeline('1661187544476975104', {
     max_results: 1,
     'tweet.fields': ['author_id', 'created_at'],
 }).then((tweets) => {
