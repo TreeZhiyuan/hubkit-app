a3c3b1ad04268f2c9ac744481b147f4953f2a8e9
zhiyuan <czy20140714@163.com>
2025-08-13 17:58:52 +0800

    debug script execute failure

--- scripts/twitter/tuserInfo.ts
+++ scripts/twitter/tuserInfo.ts
@@ -1,8 +1,17 @@
 // https://developer.x.com/en/docs/twitter-api/users/lookup/api-reference/get-users-by-username-username
 
-import { readOnlyClient } from "./common";
+import { TwitterApi } from 'twitter-api-v2';
 
+// Instantiate with desired auth type (here's Bearer v2 auth)
+const twitterClient = new TwitterApi(
+  {
+    appKey: process.env.APP_KEY || '',
+    appSecret: process.env.APP_SECRET || '',
+    accessToken: process.env.ACCESS_TOKEN || '',
+    accessSecret: process.env.ACCESS_SECRET || '',
+  }
+);
 // Play with the built in methods
-const user = await readOnlyClient.userByUsername('czydmm');
+const user = await twitterClient.v2.readOnly.userByUsername('czydmm');
 
 console.log(user);
