136532f6f71253f78cc701c8da917a5ecf8c2287
zhiyuan <czy20140714@163.com>
2025-08-16 19:00:00 +0800

    now get user info/likes/user's latest posts all done

--- scripts/twitter/posts.ts
+++ scripts/twitter/posts.ts
@@ -1,16 +1,8 @@
-import { TwitterApi } from 'twitter-api-v2';
+import { twitterV2Client } from "./common.ts";
 
-// Instantiate with desired auth type (here's Bearer v2 auth)
-const twitterClient = new TwitterApi(
-    {
-        appKey: process.env.APP_KEY || '',
-        appSecret: process.env.APP_SECRET || '',
-        accessToken: process.env.ACCESS_TOKEN || '',
-        accessSecret: process.env.ACCESS_SECRET || '',
-    }
-);
-
-const tweets = await twitterClient.v2.userTimeline('1397504889283092480', {
+// Fetching the latest tweets from a specific user
+// 1397504889283092480 哈芝咪经济
+const tweets = await twitterV2Client.userTimeline('1397504889283092480', {
     max_results: 5,
     exclude: ['retweets', 'replies'], // Optional: exclude retweets and replies
     'tweet.fields': ['created_at', 'text', 'public_metrics'], // Specify fields you want
