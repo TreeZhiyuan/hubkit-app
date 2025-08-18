1e5cf28f8e0d161acfe24981db756415b21b359e
zhiyuan <czy20140714@163.com>
2025-08-18 18:56:02 +0800

    another way to query user's top 5 post

--- scripts/twitter/posts.ts
+++ scripts/twitter/posts.ts
@@ -1,13 +1,26 @@
-import { readOnlyClient, readWriteClient } from "./common.ts";
+import { TwitterApi } from 'twitter-api-v2';
 
-// https://developer.x.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets
+// Instantiate with desired auth type (here's Bearer v2 auth)
+const twitterClient = new TwitterApi(
+    {
+        appKey: process.env.APP_KEY || '',
+        appSecret: process.env.APP_SECRET || '',
+        accessToken: process.env.ACCESS_TOKEN || '',
+        accessSecret: process.env.ACCESS_SECRET || '',
+    }
+);
 
-// zhiha 1397504889283092480
-readOnlyClient.userTimeline('1661187544476975104', {
-    max_results: 6,
-    'tweet.fields': ['author_id', 'created_at', 'text', 'public_metrics'],
-}).then((tweets) => {
-    console.log('User Tweets:', tweets);
-}).catch((error) => {
-    console.error('Error fetching user tweets:', error);
+const tweets = await twitterClient.v2.userTimeline('1397504889283092480', {
+    max_results: 5,
+    exclude: ['retweets', 'replies'], // Optional: exclude retweets and replies
+    'tweet.fields': ['created_at', 'text', 'public_metrics'], // Specify fields you want
 });
+
+for await (const tweet of tweets) {
+    console.log({
+        id: tweet.id,
+        text: tweet.text,
+        created_at: tweet.created_at,
+        metrics: tweet.public_metrics,
+    });
+}
