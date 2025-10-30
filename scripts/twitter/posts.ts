c9255f6249585b3cd32487ec0ef9a8a299b5a4c6
zhiyuan <czy20140714@163.com>
2025-10-30 09:33:34 +0800

    configure fetch x posts

--- scripts/twitter/posts.ts
+++ scripts/twitter/posts.ts
@@ -1,25 +1,43 @@
 import { twitterV2Client } from "./common.ts";
 
 // Fetching the latest tweets from a specific user
-// 1397504889283092480 哈芝咪经济
 
+const followingAccounts = [
+{
+    id: '1397504889283092480',
+    name: '哈芝咪经济'
+}, 
+// {
+//     id: '1523583150449778689',
+//     name: '擒龙捉妖.陆地神仙.菜问'
+// },
+{
+    id: '1003908754813935621',
+    name: 'Rocky'
+}
+];
 
 // Set to the start of today in UTC
 let startTime = new Date();
 startTime.setUTCHours(0, 0, 0, 0);
 
-const tweets = await twitterV2Client.userTimeline('1397504889283092480', {
-    max_results: 5,
-    start_time: startTime.toISOString(), // Tweets from today
-    exclude: ['retweets', 'replies'], // Optional: exclude retweets and replies
-    'tweet.fields': ['created_at', 'text', 'public_metrics'], // Specify fields you want
-});
+for (const account of followingAccounts) {
+    console.log(`------ fetching tweets for ${account.name} (ID: ${account.id})`);
 
-for await (const tweet of tweets) {
-    console.log({
-        id: tweet.id,
-        text: tweet.text,
-        created_at: tweet.created_at,
-        metrics: tweet.public_metrics,
+    const tweets = await twitterV2Client.userTimeline(account.id, {
+        max_results: 5,
+        start_time: startTime.toISOString(),
+        exclude: ['retweets', 'replies'],
+        'tweet.fields': ['created_at', 'text', 'public_metrics']
     });
+
+    for await (const tweet of tweets) {
+        console.log({
+            id: tweet.id,
+            text: tweet.text,
+            created_at: tweet.created_at,
+            metrics: tweet.public_metrics,
+        });
+    }
 }
+
